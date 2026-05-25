d89acb73a7e19987617fed21d79f33f4cfb19757
ashem1100 <ashkanemami2005@gmail.com>
2026-05-25 11:59:03 +0330

    add statics files, fix index.html internal urls

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from main.views import *
 
 urlpatterns = [
+    path('',index,name='index'),
+    path('contact/',contact,name='contact'),
+    path('credentials/',credentials,name='credentials'),
 
 ]
