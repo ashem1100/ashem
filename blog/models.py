5c08a0a871455764220d5e468fc618169420f176
ashem1100 <ashkanemami2005@gmail.com>
2026-05-28 20:08:24 +0330

    remake database schematic

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,25 @@
+from symtable import Class
+
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.title
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(null=True)
+    status = models.BooleanField(default=False)
+    #author
+    #main pic
+    #categories
+    #tags
+    #views
+
+class Category (models.Model):
+    name = models.CharField(max_length=200)
+    #parent
+
+
+class Tag (models.Model):
+    name = models.CharField(max_length=200)
