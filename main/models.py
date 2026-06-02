fe4246dc27c5c766042cd1e47947cd828cb6b8b0
ashem1100 <ashkanemami2005@gmail.com>
2026-06-02 10:05:02 +0330

    create and migrate cv page models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class BaseInformation(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    profile = models.ImageField(null=True)
+    cvlink =models.URLField(null=True)
+    about = models.TextField(null=True)
+    def __str__(self):
+        return self.name
+
+
+class JobExperienceInformation(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    about = models.TextField(null=True)
+    time_start = models.CharField(max_length=100)
+    time_end = models.CharField(max_length=100, default='تاکنون')
+    def __str__(self):
+        return self.title
+
+
+class EducationInformation(models.Model):
+    title = models.CharField(max_length=100)
+    academy = models.CharField(max_length=100)
+    time_start = models.CharField(max_length=100)
+    time_end = models.CharField(max_length=100, default='تاکنون')
+
+    def __str__(self):
+        return self.title
+
+class SkillInformation(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+class CertificateInformation(models.Model):
+    title = models.CharField(max_length=100)
+    certificator = models.CharField(max_length=100)
+    date = models.DateField(null=True)
