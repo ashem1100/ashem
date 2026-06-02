43c8666cf557ddf78ff153bb26631ec73ba65e59
ashem1100 <ashkanemami2005@gmail.com>
2026-06-02 11:45:40 +0330

    create and setup admin file for main app(based on models)

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from main.models import *
 # Register your models here.
+
+class BaseInformationAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    pass
+
+class JobAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'company',)
+    list_display = ('title', 'company',)
+    list_filter = ('time_start', 'time_end',)
+    pass
+
+class EducationInformationAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'academy',)
+    list_display = ('title', 'academy',)
+    list_filter = ('time_start', 'time_end',)
+    pass
+
+class SkillInformationAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'description',)
+    list_display = ('title', 'description',)
+    pass
+
+class CertificateInformationAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'certificator',)
+    list_display = ('title', 'certificator','date')
+    list_filter = ('certificator', 'date')
+
+admin.site.register(BaseInformation, BaseInformationAdmin)
+admin.site.register(JobExperienceInformation, JobAdmin)
+admin.site.register(EducationInformation, EducationInformationAdmin)
+admin.site.register(SkillInformation, SkillInformationAdmin)
+admin.site.register(CertificateInformation, CertificateInformationAdmin)
