49d8bf0a56b2d027cb7ee5a67ce5b1915e5e7cee
ashem1100 <ashkanemami2005@gmail.com>
2026-06-21 23:21:10 +0330

    add inclusion tag fo blog index category sidebar

--- blog/templatetags/blogtags.py
+++ blog/templatetags/blogtags.py
@@ -1,6 +1,8 @@
+from itertools import count
+
 from django import template
 
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -8,3 +10,17 @@ register = template.Library()
 def latestPosts():
     posts = Post.objects.filter(status=1).order_by('-publish_date')[:5]
     return {'posts': posts}
+
+
+
+@register.inclusion_tag('blog/sidebar-categories.html')
+def sidebarCategories():
+    categories = Category.objects.all()
+    posts = Post.objects.filter(status=1)
+    active_categories = {}
+    for category in categories:
+        active_categories[category.name] = posts.filter(categories=category).count()
+
+    active_categories = dict(sorted(active_categories.items(), key=lambda t: t[1], reverse=True))
+    active_categories = {cat:count for cat,count in active_categories.items() if count > 0}
+    return {'active_categories': active_categories}
